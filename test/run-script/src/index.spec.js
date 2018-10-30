7ee84eb86a1a4b8a20172f2eecd14094eb8476da
AHN HO JUNG <nb90407@navercorp.com>
2018-10-30 15:03:03 +0900

    upgrade testpad

--- test/run-script/src/index.spec.js
+++ test/run-script/src/index.spec.js
@@ -1,9 +1,12 @@
-import animals from './index.js';
+import runScript from './index.js';
 
 describe('test', () => {
   
   it('animals', () => {
-    expect(typeof animals).toEqual("object");
+    expect(typeof runScript).toEqual("object");
+    expect(typeof runScript.animals).toEqual("object")
+    expect(typeof runScript.animals.cat).toEqual("object")
+    expect(typeof runScript.animals.dog).toEqual("object")
   });
   
 });
